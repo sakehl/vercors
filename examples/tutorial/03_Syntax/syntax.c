74c13da36b795f5fc75403afee2614e35f8283a5
ArmborstL <l.armborst@utwente.nl>
2020-06-26 13:40:15 +0200

    Added examples for Tutorial chapter "Syntax"

---
+++ examples/tutorial/03_Syntax/syntax.c
@@ -0,0 +1,218 @@
+// -*- tab-width:4 ; indent-tabs-mode:nil -*-
+//:: cases TutorialSyntaxC
+//:: suite TutorialExamples
+//:: tools silicon
+//:: verdict Pass
+
+/*
+    This file contains the Examples from the Chapter "Syntax" of the VerCors tutorial 
+    (see https://github.com/utwente-fmt/vercors/wiki/Tutorial-Syntax ), ported to C.
+    This file also serves as the example for OpenCL, which is an extension of C.
+*/
+
+/// global variable used by some examples
+int y;
+
+
+void example1() {
+    int x = 2;
+    //@ assert x == 2;
+    int y = x + 3;
+    /*@
+    assert y == 5;
+    @*/
+}
+
+void example2(int arr[]) {
+    /// note: currently not supported, see Issue #453
+    /// ghost option<int> x = (arr==NULL ? None : Some(arr[0]);
+}
+
+/*@
+    /// requirements to make assertion actually pass
+    requires x==1 && y==5 && z!=NULL;
+@*/
+void example3(int x, int y, int z[]) {
+    //@ assert (\let int abs_x = (x<0 ? -x : x); y==(z==NULL ? abs_x : 5*abs_x));
+}
+
+/// example 4
+//@ requires arr != NULL;
+//@ requires (\forall* int i ; 0<=i && i<arr_len ; Perm(arr[i], read));
+//@ requires (\forall int i ; 0<=i && i<arr_len ; arr[i]>0);
+void foo(int arr[], int arr_len) {
+    /// example of a quantifier using an interval
+    //@ assert (\forall int i = 0 .. arr_len ; arr[i]>0);
+}
+
+/// example 5
+//@ requires arr != NULL;
+//@ requires Perm(arr[*], read);
+//@ requires (\forall* int i ; 0<=i && i<arr_len ; Perm(arr[i], read));
+//@ requires (\exists int i ; 0<=i && i<arr_len ; arr[i]>0);
+void foo2(int arr[], int arr_len) {
+}
+
+/*
+///    /// requirements to make assertion actually pass
+///     requires x==4 ** |s| ==1 ** s[0]==2;
+/// ghost void example6(int x, seq<int> s) {
+    /// Issue #458: \sum not supported
+    /// assert x == (\sum int i; 0<i && i<|s|; s[i]*s[i]);
+}
+*/
+
+void example7() {
+    int x = 2;
+    //@ assume x == 1;
+    int y = x + 3;
+    //@ assert y == 4;
+}
+
+/// example 8
+/*@ 
+    context Perm(y, 1);
+    requires x == 2;
+    ensures y == 5;
+@*/
+void foo3(int x) {
+    y = x + 3;
+}
+
+/// example 9
+//@ requires Perm(y, 1);
+void bar() {
+    int a = 2;
+    foo3(a);
+    a = y + 5;
+    //@ assert a == 10;
+}
+
+/// example 10
+//@ context Perm(y, 1\2);
+//@ context y == 5;
+int incr(int x) {
+    int res = x + 2*y;
+    //@ assert res == x + 10;
+    return res;
+}
+
+/// example 11
+//@ context Perm(y, 1\2);
+//@ ensures y == \old(y);
+//@ ensures \result == x + 2*y;
+int incr2(int x) {
+    return x + 2*y;
+}
+
+/// example 12
+//@ requires a>0 && b>0;
+//@ ensures \result == a*b;
+int mult(int a, int b) {
+    int res = 0;
+    //@ loop_invariant res == i*a;
+    //@ loop_invariant i <= b;
+    for (int i=0; i<b; i++) {
+        res = res+a;
+    }
+    //@ assert a == \old(a);
+    return res;
+}
+
+/// example 13
+/*@
+    requires arr != NULL;
+    requires (\forall* int i ; 0<=i && i<arr_len ; Perm(arr[i], read));
+    /// requirements to make assert true
+    requires y==5 && arr_len==1 && arr[0]==3;
+@*/
+void whatever(int x, int y, int arr[], int arr_len) {
+    x = 2;
+    //@ ghost int min = (x<y ? x : y);
+    //@ assert (\forall int i; 0<=i && i<arr_len; min<=arr[i] && arr[i] < 2*min);
+    if (arr_len > 0) {
+        x = 4;
+    }
+    /*@ ghost 
+    if (min < x) {
+        assert (\forall int i; 0<=i && i<arr_len; min<=arr[i] && arr[i] < 2*min);
+    }
+    @*/
+}
+
+/// example 14
+/*
+/// ghost methods currently not supported, see Issue #460
+requires x > 0;
+ensures cond ==> \result == x+y;
+int cond_add(bool cond, int x, int y) {
+    if (cond) {
+        return x+y;
+    } else {
+        return x;
+    }
+}
+*/
+
+//@ requires val1 > 0 && val2>0 && z==val1+val2;
+void some_method(int val1, int val2, int z) {
+    // ghost int z2 = cond_add(val2>0, val1, val2);
+    // assert z == z2;
+}
+
+
+/// example 15
+/*
+/// pure functions currently not supported, see Issue #460
+requires x > 0;
+pure int cond_add2(bool cond, int x, int y) 
+    = cond ? x+y : x;
+*/
+
+/// example 16
+//@ requires a>0 && b>0;
+//@ ensures \result == a*b;
+int mult2(int a, int b);
+// commented out body for the sake of speeding up the analysis:
+//{
+//  int res = 0;
+//  // loop_invariant res == i*a;
+//  // loop_invariant i <= b;
+//  for (int i=0; i<b; i++) {
+//    res += a;
+//  }
+//  return res;
+//}
+
+/* 
+/// pure functions currently not supported, see Issue #460
+requires a>0 && b>0;
+ensures \result == a+b;
+pure int add(int a, int b);
+*/
+
+/// example 17
+/// pure functions currently not supported, see Issue #460
+// pure inline int min(int x, int y) = (x<y ? x : y);
+
+/// example 18
+/*@
+given int x;
+given int y2;
+yields int modified_x;
+requires x > 0;
+ensures modified_x > 0;
+@*/
+int some_method2(int real_arg) {
+    int res = 0;
+    /// ...
+    //@ ghost modified_x = x + 1;
+    /// ...
+    return res;
+}
+
+void other_method2() {
+    //@ ghost int some_ghost;
+    /// ghost parameters currently not supported, see Issue #460
+    /// int some_result = some_method2(0) /* with {y2=3; x=2;} then {some_ghost=modified_x;} */;
+}
