093c99e341c29be75897a87ca4e25d2fca338765
Lars <l.b.vandenhaak@gmail.com>
2025-02-23 21:36:07 +0100

    Move from unsorted

--- src/col/vct/col/ast/family/coercion/CoerceBetweenUniqueStructImpl.scala
+++ src/col/vct/col/ast/family/coercion/CoerceBetweenUniqueStructImpl.scala
@@ -1,9 +1,8 @@
-package vct.col.ast.unsorted
+package vct.col.ast.family.coercion
 
 import vct.col.ast.CoerceBetweenUniqueStruct
 import vct.col.ast.ops.CoerceBetweenUniqueStructOps
 import vct.col.print._
 
 trait CoerceBetweenUniqueStructImpl[G] extends CoerceBetweenUniqueStructOps[G] { this: CoerceBetweenUniqueStruct[G] =>
-  // override def layout(implicit ctx: Ctx): Doc = ???
 }
--- src/col/vct/col/ast/lang/c/CUniquePointerFieldImpl.scala
+++ src/col/vct/col/ast/lang/c/CUniquePointerFieldImpl.scala
@@ -1,9 +1,9 @@
-package vct.col.ast.unsorted
+package vct.col.ast.lang.c
 
 import vct.col.ast.CUniquePointerField
 import vct.col.ast.ops.CUniquePointerFieldOps
 import vct.col.print._
 
 trait CUniquePointerFieldImpl[G] extends CUniquePointerFieldOps[G] { this: CUniquePointerField[G] =>
-  // override def layout(implicit ctx: Ctx): Doc = ???
+  override def layout(implicit ctx: Ctx): Doc = Doc.inlineSpec(Text("unique_pointer_field<") <> name <>","<> i.toString() <> ">")
 }
